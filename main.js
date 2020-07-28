bfe58beb32af6f36e8ed5087d31ff72c0d32fb46
Daniele Lasagno <danielelasagno@gmail.com>
2020-07-28 16:52:58 +0200

    Converted vars to lets and consts

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var canvas = document.getElementById('canvas')
-var ctx = canvas.getContext('2d')
-var src = ['./stone.png', './dirt.png']
-var selector = document.getElementById('selector')
-var selectedMaterial
-var images = []
-for (var f of src) {
-    var s = document.createElement('img')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const src = ['stone', 'dirt'].map(src => `./assets/textures/${src}.png`)
+const selector = document.getElementById('selector')
+let selectedMaterial
+const images = []
+for (const f of src) {
+    const s = document.createElement('img')
     s.src = f
     s.addEventListener('click', function () {
         selectedMaterial = this
@@ -21,25 +21,25 @@ canvas.addEventListener('mousemove', function (e) {
     if (e.buttons == 1 && selectedMaterial != null)
         ctx.drawImage(selectedMaterial, Math.floor(e.offsetX / 32) * 32, Math.floor(e.offsetY / 32) * 32)
 })
-var w = canvas.offsetWidth
-var h = canvas.offsetHeight
-var gridSize = 32
+const w = canvas.offsetWidth
+const h = canvas.offsetHeight
+const gridSize = 32
 
 function drawGrid() {
-    var linesX = w / gridSize
-    var linesY = h / gridSize
+    const linesX = w / gridSize
+    const linesY = h / gridSize
     ctx.lineWidth = 1
     ctx.strokeStyle = '#aeaeae'
-    for (var x = 0; x < linesX; x++) {
-        ctx.moveTo(x * gridSize, 0);
-        ctx.lineTo(x * gridSize, h);
-        ctx.stroke();
+    for (let x = 0; x < linesX; x++) {
+        ctx.moveTo(x * gridSize, 0)
+        ctx.lineTo(x * gridSize, h)
+        ctx.stroke()
     }
 
-    for (var y = 0; y < linesY; y++) {
-        ctx.moveTo(0, y * gridSize);
-        ctx.lineTo(w, y * gridSize);
-        ctx.stroke();
+    for (let y = 0; y < linesY; y++) {
+        ctx.moveTo(0, y * gridSize)
+        ctx.lineTo(w, y * gridSize)
+        ctx.stroke()
     }
 }
 drawGrid()
