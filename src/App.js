55aa7ce79e7de473fbbe13975af2661650d35b86
Daniele Lasagno <danielelasagno@gmail.com>
2020-08-06 19:03:17 +0200

    Improved drawing, needs furthere optimization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './App.css';
 
 import textures from './textures';
 import TextureBar from './components/TextureBar';
 import MapView from './components/MapView';
 
+const TextureContext = React.createContext('selectedTexture');
+
+export function useTexture() {
+  return useContext(TextureContext);
+}
+
 function App() {
   const [selectedTexture, setSelectedTexture] = useState(textures[0]);
 
   return (
+    <TextureContext.Provider value={selectedTexture}>
+
     <div className="App">
       <img className="App-selected-texture" src={selectedTexture.path} alt=""></img>
       <TextureBar onTexturePick={(texture) => setSelectedTexture(texture)} />
@@ -19,6 +27,8 @@ function App() {
       </div>
       <MapView width={20} height={20} cellSize={32} />
     </div>
+
+    </TextureContext.Provider>
   );
 }
 
--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,36 +1,71 @@
 import React, { useState } from 'react';
+import { useTexture } from '../App';
 import './MapView.css'
+
 import GridLayer from './MapGrid';
 import MapLayer from './MapLayer';
 
-import textures from '../textures'
-
 function MapView(props) {
-  let [cells, setCells] = useState([]);
+  const [mapCells, setMapCells] = useState([]);
+  const [previewCells, setPreviewCells] = useState([]);
+
+  const selectedTexture = useTexture();
 
   function handleClick(e) {
     const x = Math.floor((e.clientX - e.currentTarget.getBoundingClientRect().x) / props.cellSize);
-      const y = Math.floor((e.clientY - e.currentTarget.getBoundingClientRect().y) / props.cellSize);
+    const y = Math.floor((e.clientY - e.currentTarget.getBoundingClientRect().y) / props.cellSize);
 
-      if (!cells[x])
-        cells[x] = [];
-      if (!cells[x][y])
-        cells[x][y] = {};
-      
-      setCells(cells.map((row, i) => i !== x ? row :
-        row.map((cell, j) => j !== y ? cell :
-          { ...cell, texture: textures[0]})));
+    drawMap(x, y, { texture: selectedTexture });
+  }
+
+  function drawMap(x, y, { texture }) {
+    draw(setMapCells, x, y, { texture });
+  }
+
+  function drawPreview(x, y, { texture }) {
+    draw(setPreviewCells, x, y, { texture });
+  }
+
+  function clearPreview() {
+    setPreviewCells([]);
   }
 
   return (
-    <div className="MapView" onClick={handleClick} onMouseMove={e => {
-      if (e.buttons === 1)
-        handleClick(e);
-    }}>
-      <MapLayer width={props.width} height={props.height} cellSize={props.cellSize} cells={cells} />
+    <div
+      className="MapView"
+      onClick={handleClick}
+      onMouseOut={clearPreview}
+      onMouseMove={e => {
+        const x = Math.floor((e.clientX - e.currentTarget.getBoundingClientRect().x) / props.cellSize);
+        const y = Math.floor((e.clientY - e.currentTarget.getBoundingClientRect().y) / props.cellSize);
+
+        if (e.buttons === 1)
+          drawMap(x, y, { texture: selectedTexture });
+        else {
+          clearPreview();
+          drawPreview(x, y, { texture: selectedTexture });
+        }
+      }
+    }>
+      <MapLayer width={props.width} height={props.height} cellSize={props.cellSize} cells={mapCells} />
+      <MapLayer width={props.width} height={props.height} cellSize={props.cellSize} cells={previewCells} />
       <GridLayer width={props.width} height={props.height} cellSize={props.cellSize} />
     </div>
   );
 }
 
 export default MapView;
+
+
+function draw(setCells, x, y, { texture }) {
+  setCells(cells => {
+    if (!cells[x])
+        cells[x] = [];
+    if (!cells[x][y])
+      cells[x][y] = {};
+    
+    return cells.map((row, i) => i !== x ? row :
+      row.map((cell, j) => j !== y ? cell :
+        { ...cell, texture}));
+  });
+}
