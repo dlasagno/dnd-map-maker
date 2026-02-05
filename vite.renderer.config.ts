767c568b2799c1aa74789829ccfeecfa3613fbf5
Daniele Lasagno <danielelasagno@gmail.com>
2026-02-05 10:51:00 +0100

    Setup electron forge + vite

---
+++ vite.renderer.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config
+export default defineConfig(async () => {
+  const react = (await import("@vitejs/plugin-react")).default;
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+  };
+});
